7a2f25676afb96be957adb418535aac459bdf2aa
Engin <secondside@hotmail.com>
2014-12-24 17:23:09 +0200

    try ve except kullanimi

---
+++ tryexcept.py
@@ -0,0 +1,8 @@
+try:
+    sayi = int(raw_input("Lütfen bir sayı girin: "))
+    print "Girdiğiniz sayı", sayi
+
+except ValueError:
+    print "Lütfen harf değil, sayı girin!"
+
+print "Teşekkürler. Hoşçakalın!"
