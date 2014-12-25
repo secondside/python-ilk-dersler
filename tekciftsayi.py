f44d4caf8a63638fcd80e26d7ccfcab79b421973
Engin <secondside@hotmail.com>
2014-12-25 15:03:28 +0200

    girilen sayının tek veya cift oldugunu bulma

--- tekciftsayi.py
+++ tekciftsayi.py
@@ -1,13 +1,12 @@
+
 def tek():
-    print "girdiginiz sayi bir tek sayidir"
+    return "girdiginiz sayi bir tek sayidir"
 
 def cift():
-    print "girdiginiz sayi bir cift sayidir"
+    return "girdiginiz sayi bir cift sayidir"
+sayi = int(raw_input("lutfen bir sayi giriniz:"))
 
-sayi = raw_input("lutfen bir sayi giriniz:")
-
-if int(sayi) % 2 == 0:
+if sayi % 2 == 0:
     print cift()
-
 else:
     print tek()
